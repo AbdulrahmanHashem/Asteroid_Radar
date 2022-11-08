c9fa28cd778017f0846457bd3821b9e72a2a8b31
Abdulrhman <abdalrhmannts@gmail.com>
2022-11-08 13:55:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/asteroidradar/main/MainFragment.kt
@@ -0,0 +1,50 @@
+package com.example.asteroidradar.main
+
+import android.os.Bundle
+import android.view.*
+import androidx.core.view.MenuProvider
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModelProvider
+import com.example.asteroidradar.R
+import com.example.asteroidradar.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        val binding = FragmentMainBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+
+        binding.viewModel = viewModel
+
+//        setHasOptionsMenu(true)
+
+        with(binding){
+            requireActivity().addMenuProvider(object : MenuProvider {
+                override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                    menuInflater.inflate(R.menu.main_overflow_menu, menu)
+                }
+
+                override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                    return true
+                }
+            }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+        }
+
+        return binding.root
+    }
+
+//    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+//        inflater.inflate(R.menu.main_overflow_menu, menu)
+//        super.onCreateOptionsMenu(menu, inflater)
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        return true
+//    }
+}
